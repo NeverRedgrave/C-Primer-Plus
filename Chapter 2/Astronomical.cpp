209de52e38b66a51dffffecb38cd74fa556ec826
NeverRedgrave <ProCryonicX@yandex.ru>
2024-08-28 16:23:10 +0300

    Add files via upload

---
+++ Chapter 2/Astronomical.cpp
@@ -0,0 +1,21 @@
+﻿// Напишите программу в которой функция main вызывает определяемую пользователем функцию
+//  принимающаю в качестве аргумента расстояние в световых годах и возвращает расстояние в астрономических еденицах
+
+#include <iostream>
+using namespace std;
+double astroYear(double);
+int main()
+{
+	double lightYear;
+	cout << "Enter number of light years" << endl;
+	cin >> lightYear;
+	double result = astroYear(lightYear);
+	cout << lightYear << " light year = " << result << " astromical units" << endl;
+	return 0;
+}	
+
+double astroYear(double ly)
+{
+	double astro = 63.240;
+	return ly * astro;
+}
---
+++ Chapter 2/Times.cpp
@@ -0,0 +1,19 @@
+﻿//Напишите программу которая выдаёт запрос на ввод значений часов и минут .Функция main() должна передать эти два значения функции, иеющей тип void которая отобразит эти значения
+
+#include <iostream>
+using namespace std;
+void time(int,int);
+int main()
+{
+	int hour;
+	cout << "Enter the number of hours";
+	cin >> hour;
+	int minutes;
+	cout << "Enter rhe number of minutes";
+	cin >> minutes;
+	time(hour,minutes);
+}
+void time(int h, int m)
+{
+	cout << "Time: " << h << ":" << m;
+}
