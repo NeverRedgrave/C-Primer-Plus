39dbd474c4945e411466ee1f3c73ed5291b2377d
NeverRedgrave <ProCryonicX@yandex.ru>
2024-09-05 22:19:56 +0300

    Add files via upload

---
+++ Chapter4/Event.cpp
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая приглашает пользователя ввести три результата забега на 40 ярдов(или 40 метров, если желаете) и затем отображает эти значения и их среднее.
+
+
+
+#include <iostream>
+#include <array>
+using namespace std;
+int main()
+{
+	array<double, 4> a1;
+	cout << "Enter one position sprint: ";
+	cin >> a1[0];
+	cout << endl;
+	
+	cout << "Enter two position sprint: ";
+	cin >> a1[1];
+	cout << endl;
+
+	cout << "Enter there position sprint: ";
+	cin >> a1[2];
+	cout << endl;
+
+	double avg = (a1[0] + a1[1] + a1[2]) / 3;
+	cout << "Gold : " << a1[0] << " " << "Silver: " << a1[1] << " " << "Bronze: " << a1[2] << endl;
+	cout << "Avg:" << avg << endl;
+	
+}
---
+++ Chapter4/Flip.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+	char name;
+	char LName;
+	cout << "Enter your First name: ";
+	cin >> name;
+	cout << endl;
+	cout << "Enter your Last name: ";
+	cin >> LName;
+	cout << endl;
+
+	cout << "Here the information in a single string: ";
+}
