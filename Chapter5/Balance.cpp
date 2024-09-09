f74c050eb17acee04ecf90ba59bd211685339c0f
NeverRedgrave <ProCryonicX@yandex.ru>
2024-09-09 22:53:08 +0300

    Add files via upload

---
+++ Chapter5/Balance.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double KleoInvestition = 100;
+	double neBallacneDaphne = 100;
+	int year = 1;
+	while (KleoInvestition <= neBallacneDaphne)
+	{
+		double KleoTeamp = KleoInvestition * 0.05;
+		neBallacneDaphne += 10;
+		KleoInvestition += KleoTeamp;
+		year++;
+	}
+	cout << year;
+
+
+	return 0;
+}
