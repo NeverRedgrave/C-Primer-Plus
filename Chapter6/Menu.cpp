7d4e6c68980959ae9baa3af4c401923d5267070f
NeverRedgrave <ProCryonicX@yandex.ru>
2024-09-10 11:27:50 +0300

    Add files via upload

---
+++ Chapter6/Menu.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout << "Please enter of the following choices:" << endl;
+	cout << "a: carnivore" << " " << "c: pianist" << endl;
+	cout << "b: tree" << " " << "d: game" << endl;
+	cin.get(ch).get();
+	while (ch != 'a' || ch != 'b' || ch != 'c' || ch != 'd') {
+		if (ch == 'a') {
+			cout << "A maple is a carnivore " << endl;
+		}
+		else if (ch == 'b') {
+			cout << "A maple is a tree " << endl;
+		}
+		else if (ch == 'c') {
+			cout << "A maple is a pianist " << endl;
+		}
+		else if (ch == 'd') {
+			cout << "A maple is a game " << endl;
+		}
+		else {
+			cout << "Please enter a,b,c,d" << endl;
+		}
+		cin.get(ch).get();
+ }
+}
---
+++ Chapter6/Project@V2.cpp
@@ -0,0 +1,29 @@
+﻿// Задача: Написать программу с использованием предирективы cctype, которая читает клавиатурный ввод до символа @ и
+// повторяет его, за исключением десятичных цифр, преобразуя каждую букву верхнего регистра в букву нижнего регистра и наоборот.
+
+#include <iostream>
+#include <cctype>;
+using namespace std;
+
+int main()
+{
+	char ch;
+	cout << "Enter a line to end it using the @ symbol" << endl;
+	cin.get(ch);
+
+	while (ch != '@')
+	{
+		if (islower(ch))
+		{
+			ch = toupper(ch);
+		}
+		else if (isupper(ch)) {
+			ch = tolower(ch);
+		}
+		else {
+			ch = ' ';
+		}
+		cout << ch;
+		cin.get(ch);
+	}
+}
