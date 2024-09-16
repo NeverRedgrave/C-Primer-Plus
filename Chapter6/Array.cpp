b4943f57b7e4adb77ab9567aef7ead5412d54df6
NeverRedgrave <ProCryonicX@yandex.ru>
2024-09-16 08:18:20 +0300

    Add files via upload

---
+++ Chapter6/Array.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cctype>
+using namespace std;
+int main()
+{
+
+	int people;
+	double sum = 0;
+	cout << "Enter people: ";
+	cin >> people;
+	cout << endl;
+	cout << "Enter numbers " << endl;
+	double  *array{ new double[people] };
+	int size = 0;
+	double middele = 0;
+	for (int i = 0; i < people; i++)
+	{
+		size++;
+		cin >> array[i];
+		if (array[i] == isalpha(array[i])) {
+			break;
+		}
+		sum += array[i];
+		
+		
+	}
+	middele = sum / (size - 1);
+	int lil = 0;
+	for (int i = 0; i < people; i++)
+	{
+		if (middele > array[i])
+		{
+			lil++;
+		}
+
+	}
+	cout << "Average value: " << middele  << " Number > average " << lil - 1 << endl;
+}
