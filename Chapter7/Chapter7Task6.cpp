ac0efc1c4342ee960706ab3717a9b27753529158
NeverRedgrave <ProCryonicX@yandex.ru>
2024-09-22 20:12:57 +0300

    Add files via upload

---
+++ Chapter7/Chapter7Task6.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <algorithm>
+const int ArSize = 8;
+int Fill_array(double arr[], int ArSize);
+void Show_array(double arr[], int ArSize);
+void Reverse_array(double arr[], int ArSize);
+using namespace std;
+int main()
+{
+    double arr[ArSize];
+    cout << "The actual number of elements in the array: " << Fill_array(arr, ArSize) << endl;
+    Show_array(arr,ArSize);
+    cout << "Reverse array" << endl;
+    Reverse_array(arr, ArSize);
+    
+}
+
+int Fill_array(double arr[], int ArSize)
+{
+    int n = 0;
+    cout << "Enter number" << endl;
+    for (int i = 0; i < ArSize; i++) {
+        cin >> arr[i];
+        if (arr[i] < 0) {
+            break;
+        }
+        else {
+            n++;
+        }
+    }
+    return n;
+}
+
+void Show_array(double arr[], int ArSize) 
+{
+    for (int i = 0; i < ArSize; i++) {
+        if (arr[i] > 0) {
+            cout << "The value of the array: " << arr[i] << endl;
+        }
+    }
+}
+
+void Reverse_array(double arr[], int ArSize)
+{
+
+    reverse(arr, arr + ArSize);
+    for (int i = 0; i < ArSize; i++) {
+        if (arr[i] > 0) {
+            cout << "The value of the array: " << arr[i] << endl;
+        }
+    }
+}
---
+++ Chapter7/MiddleGarmony.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+double Middle_Garmony(int a,int b);
+int main()
+{
+	int a;
+	int b;
+	cout << "Enter namuber one: ";
+	cin >> a;
+	cout << endl;
+	cout << "Enter number two: ";
+	cin >> b;
+	cout << endl;
+	while (a != 0 && b != 0)
+	{
+		if (a != 0 && b != 0) {
+			Middle_Garmony(a, b);
+			cout << endl;
+			cout << "Enter namuber one: ";
+			cin >> a;
+			cout << endl;
+			cout << "Enter number two: ";
+			cin >> b;
+			cout << endl;
+		}
+		else {
+			Middle_Garmony(a, b);
+			cout << endl;
+			break;
+		}
+	}
+
+}
+
+double Middle_Garmony(int a ,int b)
+{
+	double garmony;
+	garmony = 2.0 * a * b / (a + b);
+	cout << "Middle Garmony: " << garmony << endl;
+	return garmony;
+}
