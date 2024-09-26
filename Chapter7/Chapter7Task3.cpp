2c5d6b020d85142c41d11a659963eb30e2b57494
NeverRedgrave <ProCryonicX@yandex.ru>
2024-09-26 09:49:05 +0300

    Add files via upload

---
+++ Chapter7/Chapter7Task3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+struct box
+{
+	char marker[40];
+	float height;
+	float width;
+	float length;
+	float volume;
+};
+
+void show_box(box b);
+float sum_volume(box b);
+
+using namespace std;
+int main()
+{
+	box b = {"Kube",20.2,10.2,1.3};
+	cout << "Structure box" << endl;
+	show_box(b);
+	cout << "Sum Volume" << endl;
+	sum_volume(b);
+}
+
+void show_box(box b) {
+	cout << "Makrek: " << b.marker << endl;
+	cout << "Height: " << b.height << endl;
+	cout << "Width: " << b.width << endl;
+	cout << "Length: " << b.length << endl;
+}
+
+float sum_volume(box b) {
+	b.volume = b.length + b.width + b.height;
+	cout << "Volume: " << b.volume << endl;
+	return b.volume;
+}
---
+++ Chapter7/Chapter7Task5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+int recurs(int number);
+using namespace std;
+int main()
+{
+	int number;
+	cout << "Enter the number: ";
+	cin >> number;
+	while (number > 0)
+	{
+		cout  << "Factorial: " << number << " equal to = " << recurs(number);
+		cout << endl;
+		cout << "Enter the number: ";
+		cin >> number;
+	}
+}
+
+int recurs(int number)
+{
+	if (number > 0) {
+		return number * recurs(number - 1);
+	}
+	else {
+		return 1;
+	}
+}
