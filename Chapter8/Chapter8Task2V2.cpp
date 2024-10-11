1df8d3841bcffed0a5234d444ec058a95552d8f2
NeverRedgrave <ProCryonicX@yandex.ru>
2024-10-11 13:09:44 +0300

    Add files via upload

---
+++ Chapter8/Chapter8Task2V2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+struct CandyBar
+{
+	char name;
+	double weidth;
+	int kaal;
+};
+
+void Candy(CandyBar& bar, const char* name = "Millenium Munch", const double weidth = 2.85, const int kaal = 350);
+
+int main()
+{
+	CandyBar* bar = new CandyBar;
+	Candy(*bar);
+}
+
+void Candy(CandyBar& bar, const char* name, const double weidth, const int kaal)
+{
+	for (int i = 0; i < strlen(name); i++)
+	{
+		bar.name = name[i];
+		cout << bar.name;
+	}
+	bar.weidth = weidth;
+	bar.kaal = kaal;
+	cout << bar.name;
+	cout << endl;
+	cout << bar.weidth;
+	cout << endl;
+	cout << bar.kaal;
+	cout << endl;
+}
---
+++ Chapter8/Chapter8Task3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+void upper(string & str);
+int main()
+{
+	string str;
+	cout << "Enter string: ";
+	cin >> str;
+	cout << endl;
+	while (str != "q")
+	{
+		cout << "Upper String: ";
+		upper(str);
+		cout << endl;
+		cout << "Enter string: ";
+		cin >> str;
+		cout << endl;
+	}
+}
+
+void upper(string & str)
+{
+	for (int i = 0; i < str.length(); i++)
+	{
+		char c = toupper(str[i]);
+		cout << c;
+	}
+}
