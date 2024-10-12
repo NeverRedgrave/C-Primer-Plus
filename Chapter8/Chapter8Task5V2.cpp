4a1766eb710387bb3cd21b0e3a1758395fd03ac3
NeverRedgrave <ProCryonicX@yandex.ru>
2024-10-12 15:41:19 +0300

    Add files via upload

---
+++ Chapter8/Chapter8Task5V2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+template <typename T>
+void max5(T * arr);
+using namespace std;
+int main()
+{
+
+	cout << "Max element int array: ";
+	int Ltemp[5] = { 11.2,44.3,102.3,21.2,4 };
+	max5(Ltemp);
+	cout << endl;
+
+	cout << "Max element double array: ";
+	double Atemp[5] = { 11.2,44.3,102.3,21.2,4 };
+	max5(Atemp);
+	cout << endl;
+}
+
+template<typename T>
+void max5(T * arr)
+{
+	T temp[5] = { 11.2,44.3,102.3,21.2,4 };
+	T max = 0;
+	for (int i = 0; i < size(temp); i++)
+	{
+		if (temp[i] > max)
+		{
+			max = temp[i];
+		}	
+	}
+	cout << max;
+}
